280f5ab1dd925d0ed40267656fb72b88b06c7d4e
Luis Vela <luismvl114@gmail.com>
2021-09-21 14:12:37 +0000

    weather component

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const Weather = ({ weather: { current } }) => {
+    
+    return (
+        <div>
+            <p>Temperature: {current.temperature}</p>
+            <img src={current.weather_icons[0]} alt='weather icon' />
+            <p>Wind: {current.wing_speed} mph direction {current.wing_dir}</p>
+        </div>
+    )
+
+    
+}
+
 const Country = ({ country }) => {
     const [weather, setWeather] = useState(null)
     const API_KEY = process.env.REACT_APP_API_KEY
 
     useEffect(() => {
         axios.get(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${country.capital}`)
-        .then(res => setWeather(res.data))
-    }, [])
-
+            .then(res => setWeather(res.data))
+    })
     return (
         <div>
             <h2>{country.name}</h2>
@@ -23,9 +35,8 @@ const Country = ({ country }) => {
             <img width={200} src={country.flag} alt="flag" />
 
             <h3>Weather in {country.capital}</h3>
-            <p>Temperature: {weather.current.temperature}</p>
-            <img src={weather.current.weather_icons[0]} />
-            <p>Wind: {weather.current.wing_speed} mph direction {weather.current.wing_dir}</p>
+            {weather ? <Weather weather={weather} /> : <p>Not found</p>}
+
         </div>
     )
 }
