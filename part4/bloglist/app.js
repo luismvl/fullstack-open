801d04a5f98c578bca47d7d8e17cf617e5bb8501
Luis Vela <luismvl114@gmail.com>
2022-03-02 18:25:59 -0400

    ëxercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+
+const app = express();
+
+logger.info('Connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch(error => {
+    logger.info('Error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
+
+
