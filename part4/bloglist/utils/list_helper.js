f77673ae89aa25397f7cbf92c121d9cf1a1a526a
Luis Vela <luismvl114@gmail.com>
2022-03-03 10:24:34 -0400

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ function dummy(blogs) {
   return 1;
 }
 
+function totalLikes(blogs) {
+  const reducer = (total, currentBlog) => total + currentBlog.likes;
+  return blogs.reduce(reducer, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
