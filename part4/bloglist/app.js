ac3aa92a8d3a9a59532266bb4e5d5f7355042d85
Luis Vela <luismvl114@gmail.com>
2023-01-04 02:29:26 -0500

    set strictQuery=false to avoid deprecation message

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const blogsRouter = require('./controllers/blogs');
+const express = require('express')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
-const app = express();
+const app = express()
 
-logger.info('Connecting to', config.MONGODB_URI);
+logger.info('Connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('Connected to MongoDB');
+    logger.info('Connected to MongoDB')
   })
-  .catch(error => {
-    logger.info('Error connecting to MongoDB:', error.message);
-  });
+  .catch((error) => {
+    logger.info('Error connecting to MongoDB:', error.message)
+  })
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', blogsRouter);
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-module.exports = app;
-
-
+module.exports = app
